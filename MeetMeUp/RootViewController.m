c1a34cbec429dce21aa35356012415fd0b57af7f
Sherrie Jones <sj24kt@comcast.net>
2015-03-23 13:12:50 -0600

    initial commit

---
+++ MeetMeUp/RootViewController.m
@@ -0,0 +1,24 @@
+//
+//  RootViewController.m
+//  MeetMeUp
+//
+//  Created by Sherrie Jones on 3/23/15.
+//  Copyright (c) 2015 Sherrie Jones. All rights reserved.
+//
+
+#import "RootViewController.h"
+
+@interface RootViewController ()
+
+@end
+
+@implementation RootViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+
+
+}
+
+@end
